d549519f7335a957699b5712b7e6161787efe2dc
alanbareiro <alanalfredobareiro@gmail.com>
2022-04-04 01:57:10 -0300

    Se agrega ruteo para pokemon por id y se trae el pokemon desde la api dependiendo el req.params.id, se lo muestra por consola, se retoca cartas

--- pokemonCards/Controller/index.js
+++ pokemonCards/Controller/index.js
@@ -2,11 +2,34 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 module.exports = {
     index: async function (req, res, next) {
+        /*let pokemona = [];
+        async function find() {
+            await fetch(`https://pokeapi.co/api/v2/pokemon/1/`)
+            .then(response => response.json()).then(pokemon =>{
+                pokemona.push(pokemon)
+            console.log(pokemon); }) 
+        }*/
+          
         let pokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
         .then(response =>response.json()).then(api=>{
             let pokemons = api.results;
-            console.log(pokemons);
-            res.render('index',{pokemons})
+            //console.log(pokemons);
+            //console.log(api);
+            //console.log(pokemona);
+            res.render('index',{pokemons/*,pokemona*/})
         })
+    },
+    pokemon: async function(req , res, next){
+        let pokeId = req.params.id;
+        let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}/`)
+        .then(response => response.json()).then(api=>{
+            // 3 abilities del pokemon por id
+            let abilities = api.abilities;
+            console.log(api);
+            //console.log(abilities);
+            res.render('pokemon',{abilities})
+        })
+        
+
     }
 }
--- pokemonCards/routes/index.js
+++ pokemonCards/routes/index.js
@@ -5,5 +5,9 @@ const controller = require('../controller/index')
 
 /* GET home page. */
 router.get('/', controller.index);
+/* GET home page. */
+router.get('/pokemon/:id', controller.pokemon);
+
+
 
 module.exports = router;
